543d16a750b665d04c2f412d7a0096f3e4a124c2
cravity <rouven.haertel@web.de>
2017-03-19 21:48:55 +0100

    Converted loop into a function

--- ex33.py
+++ ex33.py
@@ -1,13 +1,25 @@
-i = 0
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+def loop(rounds):
+    i = 0
+    while i < rounds:
+        print "At the top i is %i" % i
+        numbers.append(i)
+        i += 1
+        #print "Rounds: %i" % rounds
+        print "Numbers now: ", numbers
+        print "At the bottom i is %i" % i
 
-    i += 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+#while i < 6:
+    #print "At the top i is %d" % i
+    #numbers.append(i)
+
+    #i += 1
+    #print "Numbers now: ", numbers
+    #print "At the bottom i is %d" % i
+
+count = int(raw_input("How often: "))
+loop(count)
 
 
 #print "Literally variable numbers: %d" % numbers
