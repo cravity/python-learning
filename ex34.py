f311a01d0115d8af4e994123b1a4cc22300c6a99
cravity <rouven.haertel@web.de>
2017-03-20 19:26:41 +0100

    Added exercise 34

---
+++ ex34.py
@@ -0,0 +1,4 @@
+animals = ["bear", "python", "peacock", "kangaroo", "whale", "platypus"]
+#print ">Animal Nr. 2: %s" % animals[1]
+num_animal = int(raw_input("Number of animal: "))
+print '> %s' % animals[num_animal]
